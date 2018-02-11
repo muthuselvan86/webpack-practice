ec76b29f20d4af49df8498776709ab11a9d4d9e8
muthuselvan86 <muthuselvan86>
2018-02-11 17:50:14 -0500

    update webpack loaders

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # webpack-practice
 Basics setup and execution of webpack.
+
+To Practise:
+ - Loaders (htmi-, extract-, file-).
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const path = require('path');
+// const indexHtml = path.join(__dirname, "index.html");
+const cleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
 
     // Webpack development server setup.
     devServer: {
-        stats: "errors-only", 
+        stats: "errors-only",
         contentBase: path.join(__dirname, 'dist'), // educate, Where to serve the file from. absolute path is prefered.
         compress: true, //enable the gzip.
         port: 5000, // Default to 8080.
         host: process.env.host, //defaault to localhost.
         clientLogLevel: "info", // manages log level: none, error, warning, info(default).
-        open:true, //open the browser
+        open: true, //open the browser
         overlay: true, // to show the compilation error in browser.
     },
 
@@ -18,22 +20,53 @@ module.exports = {
     // cheap-module-eval-source-map - This is recommmened option for development.
     devtool: "cheap-module-eval-source-map",
 
-    entry: "./src/js/index.js",
+    entry: [
+        "./src/js/index.js",
+        "./index.html"
+    ],
+
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
+        filename: "bundle.[hash].js"
     },
+
     resolve: {
         modules: ['./', 'node_modules', 'src'],
-        extensions: ['.js']
+        extensions: ['.js', '.html']
     },
+
     module: {
         loaders: [{
-            test: "/\.js?$/",
-            exclude: [
-                path.resolve(__dirname, "node_modules")
-            ],
-            loader: "babel-loader"
-        }]
-    }
+                test: /\.html?$/,
+                use: [{
+                        loader: "file-loader",
+                        options: {
+
+                            name: "[name].[ext]"
+                        }
+                    }, {
+                        loader: "extract-loader"
+                    },
+                    {
+                        loader: "html-loader",
+                        options: {
+                            // attrs: ["script:src"],
+                            interpolate: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: "/\.js?$/",
+                exclude: [
+                    path.resolve(__dirname, "node_modules")
+                ],
+                loader: "babel-loader"
+            }
+        ]
+    },
+
+    plugins : [
+        new cleanWebpackPlugin('dist')
+    ]   
 };
